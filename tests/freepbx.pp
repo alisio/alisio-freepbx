be4d61439204714d2cd4675865f6242eba942e36
Antonio Alisio de Meneses Cordeiro <alisio.meneses@gmail.com>
2020-07-02 17:29:24 -0300

    Add freepbx modules

--- tests/freepbx.pp
+++ tests/freepbx.pp
@@ -23,4 +23,58 @@ yumrepo { 'irontec':
   ]:
   ensure => installed,
 }
-class {'freepbx': reboot_after_install => false }
+class {'freepbx':
+  reboot_after_install => false,
+}
+-> archive { 'Add portuguese audio prompts':
+  ensure           => present,
+  path             => '/usr/src/asterisk-sounds-core-pt-BR-sln16.zip',
+  source           => 'https://www.asterisksounds.org/pt-br/download/asterisk-sounds-core-pt-BR-sln16.zip',
+  extract          => true,
+  extract_path     => '/var/lib/asterisk/sounds/pt_BR',
+  provider         => 'wget',
+  download_options => '--continue ',
+  require          => Package['asterisk'],
+  cleanup          => true,
+  creates          => '/var/lib/asterisk/sounds/pt_BR/agent-alreadyon.sln16',
+}
+freepbx::config::repo {['standard','extended','unsupported']:}
+-> freepbx::config::module {[
+    'announcement',
+    'backup',
+    'blacklist',
+    'certman',
+    'customcontexts',
+    'callforward',
+    'callwaiting',
+    'callback',
+    # 'campon',
+    'donotdisturb',
+    'dundicheck',
+    'endpointman',
+    'fax',
+    'findmefollow',
+    'hotelwakeup',
+    'iaxsettings',
+    'ivr',
+    'manager',
+    'miscapps',
+    'miscdests',
+    'paging',
+    'parking',
+    'pinsets',
+    'presencestate',
+    'phonebook',
+    'printextensions',
+    'queues',
+    'queueprio',
+    'restapi',
+    'ringgroups',
+    'setcid',
+    'speeddial',
+    'timeconditions',
+    'ttsengines',
+    'tts',
+    'webrtc',
+  ]:
+}
